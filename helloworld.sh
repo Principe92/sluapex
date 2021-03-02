387167e3316bace8f147794a6229fb9cb454ac21
Princewill Okorie <pcokorie@gmail.com>
2021-03-02 13:33:14 -0600

    updates

--- helloworld.sh
+++ helloworld.sh
@@ -4,6 +4,8 @@
 #SBATCH -n 4
 
 module load openmpi/gcc/64/1.10.7
+rm -f /xfs2/courses/cs/csci4850/princewill.okorie/hpc/helloworld_mpi
+mpic++ /xfs2/courses/cs/csci4850/princewill.okorie/hpc/helloworld.cpp -o /xfs2/courses/cs/csci4850/princewill.okorie/hpc/helloworld_mpi
 mpirun /xfs2/courses/cs/csci4850/princewill.okorie/hpc/helloworld_mpi
 
 exit 0
