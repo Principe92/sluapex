3ab5bf55e8d06faa03a9a44d4c27da6a02b36f95
Okorie, Princewill <Princewill.Okorie@finlocker.com>
2021-05-13 11:56:14 -0500

    q2 done

--- final/matrix.cpp
+++ final/matrix.cpp
@@ -1,15 +1,88 @@
-# include <iostream>
-# include <cstdlib>	// has exit(), etc.
-# include <ctime>
-# include "mpi.h"	// MPI header file
+#include <iostream>
+#include <cstdlib> // has exit(), etc.
+#include <ctime>
+#include "mpi.h" // MPI header file
+#define MASTER 0
 
 using namespace std;
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
     int nprocs, rank;
-    
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int n = 4;
+    if (argc >= 2)
+    {
+        n = atoi(argv[1]);
+    }
+
+    int A[n][n], B[n][n], C[n][n];
+    int Apart[n], Bpart[n], Cpart[n];
+
+    if (rank == MASTER)
+    {
+        // fill(A[0], A[0] + n*n, 1);
+        // fill(B[0], B[0] + n*n, 1);
+
+        int i, j;
+
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                A[i][j] = i * n + j;
+                B[i][j] = i * n + j;
+            }
+        }
+
+        cout << "A: " << endl;
+        printArray(A, n);
+
+        cout << "B: " << endl;
+        printArray(B, n);
+    }
+
+    MPI_Scatter(&A, n, MPI_INT, &Apart, n, MPI_INT, MASTER, MPI_COMM_WORLD);
+
+    MPI_Bcast(&B, n, MPI_INT, MASTER, MPI_COMM_WORLD);
+
+    fill(Cpart[0], Cpart[0] + n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Cpart[i] += Apart[j] * B[i][j];
+        }
+    }
+
+    MPI_Gather(&Cpart, n, MPI_INT, &C, n, MPI_INT, MASTER, MPI_COMM_WORLD);
+
+    cout << "C: " << endl;
+    printArray(C, n);
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+void printArray(int *array, int size)
+{
+    int i, j;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            cout << array[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    cout << endl;
 }
